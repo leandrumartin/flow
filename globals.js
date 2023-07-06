01c5cd2e6f7d73ffddd0e0f9e04c586c404276be
Leandru Martin <leandru.martin@slu.edu>
2023-07-06 10:46:10 -0500

    yeah

--- globals.js
+++
@@ -1,5 +0,0 @@
-export const spotifyClientId = 'ab43246b21ef497899f31b2377c7ed84';
-export const lastFmApiKey = '76771f62264e37870f56ccad745f7435';
-
-// export const redirectUri = 'https://leandrumartin.github.io/flow/callback.html';
-export const redirectUri = 'http://127.0.0.1:4000/flow/callback.html'; // For testing site with Jekyll
