220faaaf76a08bb4344eda404583279f03c34e3f
Leandru Martin <leandru.martin@slu.edu>
2023-12-20 11:34:02 -0600

    Now works with playlists of length >100

--- app.js
+++ app.js
@@ -1,66 +1,79 @@
-import { SpotifyWebApi } from './spotify-web-api.js';
+import { SpotifyWebApi } from "./spotify-web-api.js";
 
-import PlaylistDisplay from './classes/PlaylistDisplay.js';
-import TrackList from './classes/TrackList.js';
-import SortFactory from './classes/SortFactory.js';
+import PlaylistDisplay from "./classes/PlaylistDisplay.js";
+import TrackList from "./classes/TrackList.js";
+import SortFactory from "./classes/SortFactory.js";
 
 let finalTrackIds;
 
 // Create Spotify API object
 var spotifyApi = new SpotifyWebApi();
-spotifyApi.setAccessToken(localStorage.getItem('access-token'));
+spotifyApi.setAccessToken(localStorage.getItem("access-token"));
 
 // Helper functions
 const updateDataDisplay = (trackNum, displayText) => {
-  let element = document.querySelector('#track' + trackNum);
+  let element = document.querySelector("#track" + trackNum);
   element.textContent = displayText;
 };
 
-const getPlaylistFromUri = (inputID) => {
+const getPlaylistIdFromUri = (inputID) => {
   let uri = document.querySelector(inputID).value.trim();
-  if (!uri.startsWith('spotify:playlist:')) {
-    throw new Error('Invalid URI');
+  if (!uri.startsWith("spotify:playlist:")) {
+    throw new Error("Invalid URI");
   }
   return uri.slice(17);
 };
 
 const displayOldTracks = (data, sort) => {
   let display = new PlaylistDisplay(data);
-  document.querySelector('#old_track_order').append(display.getDisplay(sort));
+  document.querySelector("#old_track_order").append(display.getDisplay(sort));
 };
 
 const displayNewTracks = (data, sort) => {
   let display = new PlaylistDisplay(data);
-  document.querySelector('#new_track_order').append(display.getDisplay(sort));
+  document.querySelector("#new_track_order").append(display.getDisplay(sort));
 };
 
 const reset = () => {
-  document.querySelector('#old_track_order').innerHTML = '';
-  document.querySelector('#new_track_order').innerHTML = '';
-  document.querySelector('#new_playlist_form').style.display = 'none';
+  document.querySelector("#old_track_order").innerHTML = "";
+  document.querySelector("#new_track_order").innerHTML = "";
+  document.querySelector("#new_playlist_form").style.display = "none";
 };
 
 // Main
-document.querySelector('#submit').onclick = () => {
+document.querySelector("#submit").onclick = () => {
   try {
-    let playlistUri = getPlaylistFromUri('#playlist');
+    let playlistId = getPlaylistIdFromUri("#playlist");
     spotifyApi
-      .getPlaylistTracks(playlistUri, {
+      .getPlaylistTracks(playlistId, {
         fields:
-          'items(track(id, name, album(name, images), artists(name, id), external_urls(spotify), external_ids(isrc)), is_local), ',
+          "items(track(id, name, album(name, images), artists(name, id), external_urls(spotify), external_ids(isrc)), is_local), next, offset",
       })
       .then(async (data) => {
         // Clear track displays and disable button
         reset();
-        document.querySelector('#submit').disabled = true;
+        document.querySelector("#submit").disabled = true;
 
         let sortString = document.querySelector(
           'input[name="sort_method"]:checked'
         ).value;
         let sort = SortFactory(sortString);
 
+        // Get more tracks, if any, past 100 track limit
+        let allData = data;
+        while (data.next) {
+          data = await spotifyApi.getPlaylistTracks(playlistId, {
+            fields:
+              "items(track(id, name, album(name, images), artists(name, id), external_urls(spotify), external_ids(isrc)), is_local), next, offset",
+            offset: data.offset + 100,
+          });
+          console.log(data);
+          console.log(allData.items);
+          allData.items = allData.items.concat(data.items);
+        }
+
         // Display current order of tracks
-        let trackList = new TrackList(data);
+        let trackList = new TrackList(allData);
         displayOldTracks(trackList, sort);
 
         // Get necessary track data depending on what kind of sort is being done
@@ -68,24 +81,24 @@ document.querySelector('#submit').onclick = () => {
 
         // Sort and display new order of tracks
         let separate_artists =
-          document.querySelector('#separate_artists').value;
+          document.querySelector("#separate_artists").value;
         trackList.sort(sort, separate_artists);
         // trackList.sortByGenre(separate_artists);
         displayNewTracks(trackList, sort);
 
         // Get list of final track IDs
         finalTrackIds = trackList.data.map((track) => {
-          return 'spotify:track:' + track.id;
+          return "spotify:track:" + track.id;
         });
 
         // Show options for saving new playlist
-        document.querySelector('#new_playlist_form').style.display = 'flex';
-        document.querySelector('#submit').disabled = false;
+        document.querySelector("#new_playlist_form").style.display = "flex";
+        document.querySelector("#submit").disabled = false;
       })
       .catch((err) => {
         if (err.status === 401) {
-          alert('You need to log in with Spotify again.');
-          window.open('authorize.html', '_self');
+          alert("You need to log in with Spotify again.");
+          window.open("authorize.html", "_self");
         } else {
           alert(err);
           console.error(err);
@@ -97,8 +110,8 @@ document.querySelector('#submit').onclick = () => {
 };
 
 // Saving playlist
-document.querySelector('#save_button').onclick = async () => {
-  let newPlaylistName = document.querySelector('#new_playlist_name').value;
+document.querySelector("#save_button").onclick = async () => {
+  let newPlaylistName = document.querySelector("#new_playlist_name").value;
   spotifyApi
     .getMe()
     .then((user) => {
@@ -108,10 +121,10 @@ document.querySelector('#save_button').onclick = async () => {
       return spotifyApi.addTracksToPlaylist(newPlaylist.id, finalTrackIds);
     })
     .then(() => {
-      alert('Playlist saved!');
+      alert("Playlist saved!");
     })
     .catch((err) => {
-      alert('There was a problem saving your playlist.');
+      alert("There was a problem saving your playlist.");
       console.error(err);
     });
 };
--- globals.js
+++ globals.js
@@ -1,4 +1,4 @@
-export const clientId = 'ab43246b21ef497899f31b2377c7ed84';
+export const clientId = "ab43246b21ef497899f31b2377c7ed84";
 
-export const redirectUri = 'https://leandrumartin.github.io/flow/callback.html';
-// export const redirectUri = 'http://127.0.0.1:4000/flow/callback.html'; // For testing site with Jekyll
+export const redirectUri = "https://leandrumartin.github.io/flow/callback.html";
+// export const redirectUri = "http://127.0.0.1:4000/flow/callback.html"; // For testing site with Jekyll
